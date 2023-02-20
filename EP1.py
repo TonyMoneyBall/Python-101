affb0fce3de2c6638b5dc229a1ed8436efb5cc9c
TonyMoneyBall <125905885+TonyMoneyBall@users.noreply.github.com>
2023-02-21 00:05:53 +0700

    แก้ไข Ep.1 และ อัพโหลดโฟลเดอร์ package

--- EP1.py
+++ EP1.py
@@ -1,1 +1,6 @@
 print("Hello")
+print("Hell Github")
+
+friend=["Loong","Pa Lek","Loong Dam"]
+
+print(friend[0])
---
+++ basic_package/Testmodue.py
@@ -0,0 +1,8 @@
+import info
+print(info.name)
+
+import study
+print(study.subject)
+
+from money import *
+print(my_account)
